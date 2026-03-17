9a321277acfb1a59f637a392ee2afacfe15dafcc
apple122 <peun955@gmail.com>
2026-03-18 06:17:01 +0700

    update website title to AP-Strock

--- src/components/common/PageMeta.tsx
+++ src/components/common/PageMeta.tsx
@@ -1,15 +1,15 @@
 import { HelmetProvider, Helmet } from "react-helmet-async";
 
 const PageMeta = ({
-  title,
-  description,
+  title: _title, // keeping these props to avoid breaking existing implementations
+  description: _description,
 }: {
   title: string;
   description: string;
 }) => (
   <Helmet>
-    <title>{title}</title>
-    <meta name="description" content={description} />
+    <title>AP-Strock</title>
+    <meta name="description" content="AP-Strock Management System" />
   </Helmet>
 );
 
